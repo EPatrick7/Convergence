9ff512708b30b415f0e08527b7e9af61af5797a4
Cisc0-gif <Ecorp7@protonmail.com>
2024-05-01 14:40:38 -0700

    Added randomized galaxy spawning

---
+++ Convergence/Assets/Scripts/GalaxyFogSpawner.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GalaxyFogSpawner : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject prefab;
+
+    [SerializeField]
+    private int objMax;
+
+    [SerializeField]
+    private int minVal;
+
+    [SerializeField]
+    private int maxVal;
+
+    [SerializeField]
+    private float alphaVal;
+
+    [SerializeField]
+    private List<Color> objColor = new List<Color>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //Random.InitState(42); //initiates RNG w/ seed
+
+        for (var i = 0; i < Random.Range(5, objMax); i++)
+		{
+            GameObject obj = Instantiate(prefab, new Vector3(Random.Range(minVal, maxVal), Random.Range(minVal, maxVal), 0), Quaternion.identity);
+            ParticleSystem objPS = obj.GetComponentInChildren<ParticleSystem>();
+            var col = objPS.colorOverLifetime;
+            col.enabled = true;
+
+            Gradient grad = new Gradient();
+            grad.SetKeys(new GradientColorKey[] { new GradientColorKey(objColor[Random.Range(0, objColor.Count)], 0.0f), new GradientColorKey(objColor[Random.Range(0, objColor.Count)], 1.0f) }, new GradientAlphaKey[] { new GradientAlphaKey(alphaVal, 0.0f), new GradientAlphaKey(alphaVal, 1.0f) });
+
+            col.color = grad;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
