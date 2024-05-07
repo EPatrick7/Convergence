f83fa62f83a914220cd028481f70b81357cc6819
Eric Patrick <ericmpatrick777@gmail.com>
2024-05-07 16:39:40 -0700

    Fixed Build-Only Error

--- Convergence/Assets/Scripts/PlayerPixelManager.cs
+++ Convergence/Assets/Scripts/PlayerPixelManager.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using Unity.VisualScripting;
-using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using static UnityEngine.GraphicsBuffer;
