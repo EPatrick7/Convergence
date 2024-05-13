8eb8dc5a96004d9a62ba34dea5163b9eb316a06b
Eric Patrick <ericmpatrick777@gmail.com>
2024-05-13 16:55:13 -0700

    Fixed UI

--- Convergence/Assets/Scripts/CinematicBars.cs
+++ Convergence/Assets/Scripts/CinematicBars.cs
@@ -43,7 +43,7 @@ public class CinematicBars : MonoBehaviour
         topTween = topBar.DOSizeDelta(new Vector2(topBar.sizeDelta.x, height), duration);
         bottomTween = bottomBar.DOSizeDelta(new Vector2(bottomBar.sizeDelta.y, height), duration);
 
-        hudTween = playerHud.DOLocalMoveY(-800, duration);
+        hudTween = playerHud.DOLocalMoveY(-367, duration);
 
         topTween.Play();
         bottomTween.Play();
@@ -61,7 +61,7 @@ public class CinematicBars : MonoBehaviour
 
         topTween = topBar.DOSizeDelta(new Vector2(topBar.sizeDelta.x, 0f), duration);
         bottomTween = bottomBar.DOSizeDelta(new Vector2(bottomBar.sizeDelta.y, 0f), duration);
-        hudTween = playerHud.DOLocalMoveY(-375, duration);
+        hudTween = playerHud.DOLocalMoveY(-150, duration);
 
         topTween.Play();
         bottomTween.Play();
